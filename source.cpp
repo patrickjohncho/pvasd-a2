4076e1779e4f22a2818224d4ae9490ce45ebe70b
Patrick Cho <pcho3098@conestogac.on.ca>
2024-09-13 16:02:36 -0400

    Initial Commit

---
+++ source.cpp
@@ -0,0 +1,11 @@
+// Programmer: Patrick Cho
+// Class: Project V: Advanced Software Development
+// Date: 9/13/2024
+// Project: Assignment 2
+// Filename: source.cpp
+// Description: This file contains the source code for Assignment 2
+
+int main()
+{
+	return 1;
+}
